e3ddab9ad9a25eaf2ac8494ca09bd1dcecc6317a
maxime.polese <maxime.polese@le-campus-numerique.fr>
2024-05-29 09:27:30 +0200

    Exemple commentaires + ReadMe

--- README.md
+++ README.md
@@ -1,16 +1,50 @@
-# craftcorner
+# Craftcorner
+
+CraftCorner is a modern e-commerce platform built with Vue 3 and Vite. It provides a seamless shopping experience for
+users, allowing them to browse and purchase a wide variety of artisanal products.
+
+## Features
+
+- Product Listing: Displays a list of available products with detailed descriptions.
+- User Profiles: Each artisan has a profile where they can showcase their products.
+- Shopping Cart: Users can add products to a shopping cart and proceed to checkout.
+- Search Functionality: Users can search for specific products.
+
+## Tech Stack
+
+- Vue 3: A progressive JavaScript framework for building user interfaces.
+- Vite: A build tool that aims to provide a faster and leaner development experience for modern web projects.
+- Vue Router: The official router for Vue.js.
+- Vueuse/core: A collection of Vue Composition API utilities.
 
 This template should help get you started developing with Vue 3 in Vite.
 
 ## Recommended IDE Setup
 
-[VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur).
+[VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and
+disable Vetur).
 
 ## Customize configuration
 
 See [Vite Configuration Reference](https://vitejs.dev/config/).
 
 ## Project Setup
+
+To get started with the project, follow these steps:
+
+1. Clone the repository:
+
+```sh
+git clone https://github.com/MaximePolese/craftcorner.git
+```
+
+2. Navigate to the project directory:
+
+```sh
+cd craftcorner
+```
+
+3. Install the dependencies:
 
 ```sh
 npm install
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,30 +1,35 @@
 <script setup>
-import { useFetch } from '@vueuse/core'
-import { useRoute } from 'vue-router'
-import { onMounted } from 'vue'
-import { useAppStore } from '@/stores/appStore'
+// Importing necessary modules and components
+import { useFetch } from '@vueuse/core' // A composable for fetching data
+import { useRoute } from 'vue-router' // A composable for accessing the current route
+import { onMounted } from 'vue' // A lifecycle hook that is called after the component is mounted
+import { useAppStore } from '@/stores/appStore' // A composable for accessing the application store
 
-import ProductCard from '@/components/ProductCard.vue'
-import Carousel from '@/components/Carousel.vue'
+import ProductCard from '@/components/ProductCard.vue' // A component for displaying a product
+import Carousel from '@/components/Carousel.vue' // A component for displaying a carousel
 
-const route = useRoute()
-const store = useAppStore()
+// Using the composables
+const route = useRoute() // Get the current route
+const store = useAppStore() // Get the application store
 
+// Set the document title when the component is mounted
 onMounted(() => {
   document.title = `${route.name} - ${store.appName}`
 })
 
+// Fetch the products data
 const { data: products } = useFetch('https://fakestoreapi.com/products?limit=21').json()
 </script>
 
 <template>
   <div class="flex justify-center">
     <div class="container">
-      <Carousel />
+      <Carousel /> <!-- Display the carousel -->
       <h1 class="text-3xl pl-28 pt-10">Meilleures ventes</h1>
       <div class="flex flex-col items-center py-10">
         <div>
           <ul role="list" class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+            <!-- Loop over the products and display each one in a ProductCard -->
             <li v-for="product in products" :key="product.id">
               <RouterLink :to="`/product/${product.id}`">
                 <ProductCard
