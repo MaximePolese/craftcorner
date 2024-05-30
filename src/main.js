0664e0a27c34f2b4fca0e416b3cd401a83292266
maxime.polese <maxime.polese@le-campus-numerique.fr>
2024-05-30 11:43:09 +0200

    persist true

--- src/main.js
+++ src/main.js
@@ -2,13 +2,18 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import createPersistedState from 'pinia-plugin-persistedstate'
+
 
 import App from './App.vue'
 import router from './router'
 
+const pinia = createPinia()
 const app = createApp(App)
 
-app.use(createPinia())
+pinia.use(createPersistedState)
+
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
