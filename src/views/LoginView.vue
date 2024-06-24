75eb714079bae855ef5f641c0676e9b4cc97571e
MaximePolese <maxime.polese@le-campus-numerique.fr>
2024-06-24 11:10:52 +0200

    login plus joli

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -24,28 +24,30 @@ const submitForm = () => {
 </script>
 
 <template>
-  <div class="login p-10">
-    <div class="card shrink-0 w-full max-w-sm bg-base-100">
-      <form class="card-body" @submit.prevent="submitForm">
-        <div class="form-control">
-          <label class="label">
-            <span class="label-text">Email</span>
-          </label>
-          <input type="text" placeholder="username" class="input input-bordered" v-model="email" required />
-        </div>
-        <div class="form-control">
-          <label class="label">
-            <span class="label-text">Password</span>
-          </label>
-          <input type="password" placeholder="password" class="input input-bordered" v-model="password" required />
-          <label class="label">
-            <a href="#" class="label-text-alt link link-hover">Forgot password?</a>
-          </label>
-        </div>
-        <div class="form-control mt-6">
-          <button class="btn btn-primary" type="submit">Login</button>
-        </div>
-      </form>
+  <div class="flex items-center justify-center">
+    <div class="login p-10">
+      <div class="card shrink-0 w-full max-w-sm bg-base-100">
+        <form class="card-body" @submit.prevent="submitForm">
+          <div class="form-control">
+            <label class="label">
+              <span class="label-text">Email</span>
+            </label>
+            <input type="text" placeholder="username" class="input input-bordered" v-model="email" required />
+          </div>
+          <div class="form-control">
+            <label class="label">
+              <span class="label-text">Password</span>
+            </label>
+            <input type="password" placeholder="password" class="input input-bordered" v-model="password" required />
+            <label class="label">
+              <a href="#" class="label-text-alt link link-hover">Forgot password?</a>
+            </label>
+          </div>
+          <div class="form-control mt-6">
+            <button class="btn btn-custom-primary" type="submit">Login</button>
+          </div>
+        </form>
+      </div>
     </div>
   </div>
 </template>
